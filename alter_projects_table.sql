e963d6ab3560ce5f5883201dce3d3ef8daf67291
mishrakm <itsamitmishra@yahoo.com>
2025-09-09 13:32:39 +0530

    remove unused

--- alter_projects_table.sql
+++
@@ -1,5 +0,0 @@
--- SQL to alter the projects table to add new fields
-ALTER TABLE projects 
-ADD COLUMN git_project_id INT,
-ADD COLUMN branch TEXT,
-ADD COLUMN trigger_token VARCHAR(255);
--- create_project_properties_table.sql
+++
@@ -1,9 +0,0 @@
--- SQL to create the project_properties table
-CREATE TABLE project_properties (
-    property_id INT AUTO_INCREMENT PRIMARY KEY,
-    project_id INT NOT NULL,
-    property_name VARCHAR(255) NOT NULL,
-    property_value TEXT NOT NULL,
-    selection_type ENUM('single', 'multiselect') NOT NULL DEFAULT 'single',
-    FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
-);
